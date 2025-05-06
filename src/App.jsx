818a5c9a35d5363d041d4a37afb4819bda9c3795
Romit Ghosh <romitghoshrg@gmail.com>
2025-05-06 17:50:09 +0530

    added new code

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ function App() {
   return (
     <>
       <h1>Romit's React App.</h1>
+      <h2>Welcome to my React App</h2>
     </>
   )
 }
 
 export default App
+// /home/ubuntu/actions-runner/_work/cicd-react-githubaction/cicd-react-githubaction/dist
